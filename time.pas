6854159bbca2e83b2693f672a0248e77a709f963
Юлия Грошева <gros@sfedu.ru>
2023-09-30 08:19:45 +0300

    Fixed days in year

--- time.pas
+++ time.pas
@@ -2,8 +2,12 @@
   var n:=ReadInteger('Введите год:');
   assert(n>0);
   if ((n mod 4 =0) or (n mod 100 =0)) and (n mod 400<>0) then begin
-    print('Високосный')
+    print('Високосный');
+    print('кол-во дней 366')
   end
-  else
-    print('Не високосный')
+  else begin
+    print('Не високосный');
+    
+    print('кол-во дней 365');
+  end;
 end.
