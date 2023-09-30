9401661b149f5a6ba1da6be140975e3aa90e1522
Юлия Грошева <gros@sfedu.ru>
2023-09-30 07:51:39 +0300

    324rdhge

---
+++ hello.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var n:=ReadInteger('Введите число:');
+  if (n>=4) and (n<11) then
+    print('Доброе утро, мир!')
+  else if (n>=11) and (n<=16) then
+    print('Добрый день, мир!')
+  else if (n>=17) and (n<=22) then
+    print('Добрый вечер, мир!')
+  else
+  print('Доброй ночи,мир!');
+end.
